e3ece2eb6091dbed93fba1da2c42d32a1e2771b2
Your Name <you@example.com>
2026-06-04 22:38:39 +0500

    Modified templates to display posts from database.

--- blog/static/blog/js/comments/comment_core.js
+++ blog/static/blog/js/comments/comment_core.js
@@ -339,7 +339,7 @@ document.addEventListener('click', function (e) {
         const comment = cancelBtn.closest('.comment')
         const textarea = comment.querySelector('.edit-input')
 
-        textarea.style.height = '44px'
+        textarea.style.height = '0px'
 
         comment.querySelector('.comment-text').style.display = 'block'
         comment.querySelector('.edit-form-wrapper').style.display = 'none'
