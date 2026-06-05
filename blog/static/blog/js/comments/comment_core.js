1e5b24d430dbc2c6352440282a645225076a3f6b
Your Name <you@example.com>
2026-06-06 01:27:14 +0500

    Modified templates to display posts from database.

--- blog/static/blog/js/comments/comment_core.js
+++ blog/static/blog/js/comments/comment_core.js
@@ -390,11 +390,8 @@ document.addEventListener('click', function (e) {
 
         e.preventDefault()
 
-        const comment =
-            toggleReplies.closest('.comment')
-
         const repliesContainer =
-            comment.nextElementSibling
+            toggleReplies.nextElementSibling
 
         if (
             repliesContainer &&
@@ -403,16 +400,18 @@ document.addEventListener('click', function (e) {
 
             repliesContainer.classList.toggle('hidden-replies')
 
+            toggleReplies.classList.toggle('open')
+
             const isHidden =
                 repliesContainer.classList.contains('hidden-replies')
 
             const count =
                 repliesContainer.querySelectorAll('.comment.reply').length
 
-            toggleReplies.textContent =
+            toggleReplies.innerHTML =
                 isHidden
-                    ? `Ответы (${count})`
-                    : `Скрыть ответы`
+                    ? `<span class="toggle-arrow"></span> Ответы (${count})`
+                    : `<span class="toggle-arrow"></span> Скрыть ответы`
         }
 
         return
