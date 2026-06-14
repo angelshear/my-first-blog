7957f87149d023d4693e7f48313f5c32b65ef685
Your Name <you@example.com>
2026-06-14 23:56:34 +0500

    Modified templates to display posts from database.

--- blog/forms.py
+++ blog/forms.py
@@ -1,29 +1,30 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django_select2.forms import Select2MultipleWidget
 
 from .models import (
     Post,
     Comment
 )
 
-class PostForm(forms.ModelForm):
 
-    tags = forms.CharField(
-        required=False,
-        help_text="Через запятую"
-    )
+# forms.py
+
+class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = (
             'title',
             'text',
-            'image',
             'category',
             'tags',
+            'image',
         )
+
+        widgets = {
+            'tags': forms.HiddenInput()
+        }
 
 
 class CommentForm(forms.ModelForm):
